c5f84cc78c8935839213b4336f4d4a4ae2de938f
garojo <garojo@alumnatflorida.es>
2026-01-27 13:17:27 +0100

    arreglanndo controladores no van :)

--- views/iGestor.php
+++ views/iGestor.php
@@ -5,4 +5,5 @@ public interface iGestor
     public function obtenerTodos(): array;
     public function guardar(EntidadEstelar $entidad): void;
     public function eliminar(float $id): void;
+    public function editar(float $id, EntidadEstelar $entidad): void;
 }
